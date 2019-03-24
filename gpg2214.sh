39290d1add9870fc2af14b17a56b75da4c40049d
murphy <mac3iii@gmail.com>
2019-03-24 06:50:20 -0400

    Add files via upload

---
+++ gpg2214.sh
@@ -0,0 +1,20 @@
+mkdir ~/Downloads
+cd ~/Downloads
+version=gnupg-2.2.14
+wget https://gnupg.org/ftp/gcrypt/gnupg/$version.tar.bz2
+wget https://gnupg.org/ftp/gcrypt/gnupg/$version.tar.bz2.sig
+tar xf $version.tar.bz2
+cd $version
+#sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y libldap2-dev
+sudo apt-get install -y gtk+-2
+sudo apt-get install -y rng-tools
+sudo apt-get install -y libbz2-dev
+sudo apt-get install -y zlib1g-dev
+sudo apt-get install -y libgnutls28-dev  #note libgnutls30 avail, not -dev
+sudo apt-get install -y libsqlite3-dev
+sudo apt-get install -y libreadline-dev
+sudo apt-get install -y pcscd scdaemon
+sudo apt-get install -y pinentry-tty
+sudo make -f build-aux/speedo.mk INSTALL_PREFIX=/usr/local speedo_pkg_gnupg_configure='--enable-g13 --enable-wks-tools --with-pinentry-pgm=/usr/bin/pinentry-tty' native
+sudo ldconfig
